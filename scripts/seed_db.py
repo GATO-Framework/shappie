dd6662fdf709ada68e31e19f3d23b4e48b4cd8ab
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-06-26 22:08:06 -0700

    add layers in JSON

--- scripts/seed_db.py
+++ scripts/seed_db.py
@@ -1,16 +1,15 @@
 import asyncio
 import json
-import pathlib
 
 import motor.motor_asyncio
 
 
 class DatabaseSeeder:
-    def __init__(self, url: str, db_name: str, layer_dir: str,
+    def __init__(self, url: str, db_name: str, layer_file: str,
                  persona_file: str, message_file: str):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(url)
         self._db = self._client[db_name]
-        self._layer_dir = layer_dir
+        self._layer_file = layer_file
         self._persona_file = persona_file
         self._message_file = message_file
 
@@ -42,15 +41,9 @@ class DatabaseSeeder:
             await self._db['messages'].insert_many(messages)
 
     async def _insert_layers(self):
-        layer_files = pathlib.Path(self._layer_dir).glob('*.md')
-        layers = []
-
-        for layer_file in layer_files:
-            with open(layer_file, 'r') as f:
-                layer = {"name": layer_file.stem, "content": f.read()}
-                layers.append(layer)
-
-        await self._db['layer-info'].insert_many(layers)
+        with open(self._layer_file, 'r') as f:
+            messages = json.load(f)
+            await self._db['layer-info'].insert_many(messages)
 
 
 def seed_db():
@@ -58,13 +51,13 @@ def seed_db():
     seeder = DatabaseSeeder(
         url="mongodb://localhost:27017",
         db_name="GATO",
-        layer_dir="layers",
-        persona_file="GATO.personas.json",
-        message_file="GATO.messages.json",
+        persona_file="../data/seed/GATO.personas.json",
+        message_file="../data/seed/GATO.messages.json",
+        layer_file="../data/seed/GATO.layer-info.json",
     )
 
     # Run the seeder
-    asyncio.run(seeder._clear_database())
+    asyncio.run(seeder.seed())
 
 
 if __name__ == '__main__':
